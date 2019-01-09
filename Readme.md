65833fbf1c638e5582d091bc74e4bf6a1fb185c8
Florian <florian@computer.local>
2019-01-09 03:12:40 +0100

    version 0.2, added Readme and License

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Flowfield
+
+Idea and parts of the code from The Coding Train (Coding Challenge #24, https://www.youtube.com/watch?v=BjoM9oKOAKY).
+
+Watch demo here: https://afferore43.github.io/flowfield/
+
+Press [?] for a list of available commands.
--- sketch.js
+++ sketch.js
@@ -2,43 +2,56 @@ p5.disableFriendlyErrors = true;
 
 var zMove = 0.0;
 var xMove = 0.0;
+var yMove = 0.0;
 
 var scl = 0.001;
+var keySpeed = 0.05;
+var zSpeed = 0.005;
+var xSpeed = scl;
+
+var moveNoise = true;
+var moveParticles = true;
 
 var particles = [];
-var particleCount = 1500;
+var particleCount = 1000;
 
 var showInfo = false;
 var showHelp = false;
 
 function setup() {
     document.body.style.backgroundColor = "black";
-    createCanvas(windowWidth, windowHeight);
-    for (var i = 0; i < 10; i++) {
-        particles[i] = new Particle();
-    }
+    
     colorMode(HSB, 255);
+    
+    createCanvas(windowWidth, windowHeight);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+
+
 function calculateForce(x,y) {
-    return noise(x*scl+xMove,y*scl,zMove);
+    return noise(x*scl+xMove,y*scl+yMove,zMove);
 }
 
 function draw() {
-    if (particles.length < particleCount) {
-        addParticles(1);
-    }
-    zMove += 0.01;
-    xMove += 0.001;
-    background(0,0,0,10);
-    
-    for(var i=0; i<particles.length; i++) {
-        particles[i].calculateForce();
-        particles[i].show();
-        particles[i].update();
-        particles[i].edges();
+    if(moveParticles) {
+        if (particles.length < particleCount) {
+            addParticles(1);
+        }
+        if(moveNoise) {
+            zMove += zSpeed;
+            xMove += xSpeed;
+        }
+        background(0,0,0,10);
+
+        for(var i=0; i<particles.length; i++) {
+            particles[i].calculateForce();
+            particles[i].update();
+            particles[i].show();
+            particles[i].edges();
+        }
     }
     if(showInfo) {
         var h = 40;
@@ -55,7 +68,7 @@ function draw() {
     }
     
     if(showHelp) {
-        t = ["keyboard controls","","[f] fullscreen on/off","[i] show/hide info", "[?] show/hide keyboard controls", "[+/-] add/delete 100 points"]
+        t = ["keyboard controls","","[f] fullscreen on/off","[i] show/hide info", "[?] show/hide keyboard controls", "[+/-] add/delete 100 points","[arrows] move flowfield","[space] stop/star point movement","[n] stop/star noise movement","[r] set noise to random position"]
         var sx = 20;
         var sy = 20;
         
@@ -85,18 +98,40 @@ function deleteParticles(num) {
 }
 
 function keyPressed() {
-    console.log(keyCode)
+    console.log(keyCode);
   if (keyCode === 70) { // F
     var fs = fullscreen();
     fullscreen(!fs);
   } else if (keyCode === 73) { // I
       showInfo = !showInfo;
+      if(!showInfo) {
+          moveParticles = true;
+      }
   } else if (keyCode === 187) { // +
       addParticles(100);
   } else if (keyCode === 189) { // -
       deleteParticles(100);
   } else if (keyCode === 191) { // ?
       showHelp = !showHelp;
+      if(!showHelp) {
+          moveParticles = true;
+      }
+  } else if (keyCode === UP_ARROW) {
+    yMove += keySpeed;
+  } else if (keyCode === DOWN_ARROW) {
+    yMove -= keySpeed;
+  } else if (keyCode === RIGHT_ARROW) {
+    xMove -= keySpeed;
+  } else if (keyCode === LEFT_ARROW) {
+    xMove += keySpeed;
+  } else if (keyCode === 32) { // Space
+    moveParticles = !moveParticles;
+  } else if (keyCode === 78) { // N
+    moveNoise = !moveNoise;
+  } else if (keyCode === 82) { // R
+    xMove = random();
+    yMove = random();
+    zMove = random();
   }
 }
 
